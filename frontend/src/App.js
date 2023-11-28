1497792d1a9ab7d16620c050b111969a62c69f34
Hemalathas25 <hemalathasm04@gmail.com>
2023-11-28 18:09:21 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,20 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+//import list from "./components/list";
+
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+  <>
+   <Header />
+    <main className="py-3">
+      <Container>
+        <HomeScreen />
+      </Container>
+    </main>
+    <Footer />
+  </>
+  );
 };
 export default App;
