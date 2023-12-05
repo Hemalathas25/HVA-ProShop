91495a7ebc81d400f049674b9d1ef060e34c5814
Hemalathas25 <hemalathasm04@gmail.com>
2023-12-05 18:59:44 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+//const cors = import('cors');
+import cors from 'cors';
+connectDB(); // Connect to MongoDB
+
 const port = process.env.PORT || 5000;
 
-connectDB(); // Connect to MongoDB
+
 const app = express();
+app.use(cors())
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded ({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) =>{
-    res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
